422fc7ff3350dc2575ca17cb327a10ef553b4ec7
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-09-11 09:40:58 +0200

    Add files via upload

---
+++ svanTipuricDrugiZad/svanTipuricDrugiZad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricDrugiZad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s;
+            int m;
+            int o;
+            Console.WriteLine("Upisite broj sekundi: " );
+            s = Convert.ToInt32(Console.ReadLine());
+            m = s / 60;
+            o = s % 60;
+            Console.WriteLine("To je " + m + " minute i " + o + " sekundi.");
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ svanTipuricDrugiZad/svanTipuricTreciZadatak/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricTreciZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            Console.WriteLine("Upisite broj: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (b / 4 == 0) { Console.WriteLine("Broj je djeljiv sa 4.") }
+            else if (b / 6 == 0) { Console.WriteLine("Broj je djeljiv sa 6.") }
+            else if (b / 4 == 0 && b / 6 == 0) { Console.WriteLine("Broj je djeljiv sa 4 i sa 6;") }
+            else 
+            { Console.WriteLine("Broj nije djeljiv ni sa 4 ni sa 6.")};
+
+            
+            Console.ReadKey();
+
+
+
+
+
+                
+                    
+
+            
+        }
+    }
+}
